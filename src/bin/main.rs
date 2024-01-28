2ca7f881eb38b2dda3504177c34a6ffdc13065bc
schmettow <schmettow@web.de>
2024-01-28 20:45:49 +0100

    + using the full thread builder pipeline + to collect final responses from Yex thread

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,6 @@
+use yex::{sleep, Duration};
 use yex::session::{Participant, Experiment,Session};
+use yex::trial::Observation;
 pub use std::sync::mpsc::{channel, Sender, Receiver};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -9,7 +11,17 @@ fn main() {
     let part = Participant::default();
     println!("Hello {}.", part.id);
     let session = Arc::new(Mutex::new(Session::new(exp, part)));
-    thread::spawn(move || {
-        yex::demo(session);
-    });
+    // let mut obs: Option<Vec<Observation>>;
+    
+    let builder = std::thread::Builder::new();
+    let join_handle = 
+        builder.spawn(move || {
+            yex::demo(session)
+        }).unwrap().join();
+    match join_handle {
+        Ok(obs) => 
+            {println!("{} observations collected", obs.len())},
+        Err(e) => println!("Session failed")
+        
+    }
 }

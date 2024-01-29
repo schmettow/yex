661f466f89a160f1ccab67cb773ed76e4c197501
schmettow <schmettow@web.de>
2024-01-29 21:52:20 +0100

    + added a simple event listener in a separate thread. + comets

--- src/bin/yex_proto.rs
+++ src/bin/yex_proto.rs
@@ -1,17 +1,26 @@
 use yex::session::{Participant, Experiment,Session};
-//use std::sync::mpsc::{channel, Sender, Receiver};
+use yex::output::YexEvent;
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::sync::{Arc, Mutex};
-use std::thread::Builder;
+use std::thread::{self, Builder};
 
 fn main() {
+    // Dummy experiment
     let exp = Experiment::default();
     println!("Exp with {} blocks", exp.blocks.len());
+    // Dummy participant
     let part = Participant::default();
     println!("Hello {}.", part.id);
+    // Creating the session
     let session = Arc::new(Mutex::new(Session::new(exp, part)));
+    // Starting the event recorder channel
+    let (event_snd, event_rec)  = channel::<YexEvent>();
+    // Detached mock task to receive and print the events
+    thread::spawn(move || {loop {match event_rec.recv() {Ok(r) => {println!("{:?}",r)}, _ => {}} }});
+    // Building the session thread, because we want it to return a value
     let builder = Builder::new();
     let join_handle = 
-        builder.spawn(move || {yex::demo(session)}).unwrap();
+        builder.spawn(move || {yex::demo(session, event_snd)}).unwrap();
     match join_handle.join() {
         Ok(obs) => 
             {println!("{} observations collected", obs.len())},

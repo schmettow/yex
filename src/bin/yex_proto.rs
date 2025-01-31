3a72c672f8548cfb97e27b6ae6635f4ae5fabf70
schmettow <schmettow@web.de>
2025-01-31 22:06:28 +0100

    + code cleaning + trying to build the thread using a builder

--- src/bin/yex_proto.rs
+++ src/bin/yex_proto.rs
@@ -1,8 +1,10 @@
+use yex;
 use yex::session::{Participant, Experiment,Session};
 use yex::output::YexRecord;
-use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex};
 use std::thread::{self, Builder};
+use log::{debug, info, log};
+use polars as pl;
 
 fn main() {
     // Dummy experiment
@@ -14,20 +16,30 @@ fn main() {
     // Creating the session
     let session = Arc::new(Mutex::new(Session::new(exp, part)));
     // Starting the event recorder channel
-    let (event_snd, event_rec)  = channel::<YexRecord>();
-    // Detached mock task to receive and print the events
+    let (event_snd, event_rec)  = yex::channel::<YexRecord>();
+    let event_rec_ui = event_rec.clone();
+
+    // Detached task to receive events
     thread::spawn(move || {
-        loop {match event_rec.recv() {
-            Ok(r) => {println!("{:?}",r)}, 
-            _ => {}}}});
-    // Building the session thread, because we want it to return a value
+        loop {
+            match event_rec.recv() {
+                Ok(r) => {println!("{:?}",r)},
+                _ => {}}}});
+
+    thread::spawn(move || {
+        loop {
+            match event_rec_ui.recv() {
+                Ok(r) => {info!("{:?}",r)},
+                _ => {}}}});
+
+    // Building the session thread, because we want it to return a value, the data frame
     let builder = Builder::new();
     let join_handle = 
         builder.spawn(move || {yex::demo(session, event_snd)}).unwrap();
     match join_handle.join() {
-        Ok(obs) => 
-            {println!("{} observations collected", obs.len())},
-        Err(_) => println!("Session failed")
-        
+        Ok(obs) => {
+            println!("{} observations collected", obs.len())
+            },
+        Err(_)                  => println!("Session failed")
     }
 }
